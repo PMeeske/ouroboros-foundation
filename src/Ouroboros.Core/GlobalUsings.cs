44988b450dd5e37c2e61fe8f47d3e00467d5415f
meeske <philip.meeske@adesso.de>
2026-02-11 13:09:59 +0100

    Add comprehensive System global usings

--- src/Ouroboros.Core/GlobalUsings.cs
+++ src/Ouroboros.Core/GlobalUsings.cs
@@ -4,8 +4,17 @@
 
 // System namespaces (ImplicitUsings is disabled in csproj)
 global using System;
+global using System.Collections.Concurrent;
 global using System.Collections.Generic;
 global using System.Collections.Immutable;
+global using System.Diagnostics;
+global using System.IO;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Reflection;
+global using System.Text;
+global using System.Text.Json;
+global using System.Text.RegularExpressions;
 global using System.Threading;
 global using System.Threading.Tasks;
 
